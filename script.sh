a32950d5bbc3765d104be3046a0876e49a0aa47a
Szymon Kaczmarek <kaczmarek100@tlen.pl>
2023-06-26 23:46:29 +0200

    Utworzony dalszą część kodu, która generuje 100 plików

--- script.sh
+++ script.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-# Sprawdzanie, czy pierwszy argument to "--date"
+# Nazwa skryptu
+script_name=$(basename $0)
+
+# Sprawdzanie, czy pierwszy argument to "--date" lub "--logs"
 if [ "$1" == "--date" ]; then
     # Wyświetlanie bieżącej daty i godziny
     echo $(date)
+elif [ "$1" == "--logs" ]; then
+    # Tworzenie 100 plików logx.txt
+    for ((i=1; i<=100; i++)); do
+        # Nazwa pliku
+        file_name="log$i.txt"
+        
+        # Zapisywanie informacji do pliku
+        echo "Nazwa pliku: $file_name" > $file_name
+        echo "Nazwa skryptu: $script_name" >> $file_name
+        echo "Data: $(date)" >> $file_name
+    done
 fi
